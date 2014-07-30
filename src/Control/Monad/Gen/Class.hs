8933c98d7573c98c58701ee8b29ebc89a4fa6541
Danny Gratzer <danny.gratzer@gmail.com>
2014-07-30 10:13:08 -0500

    Added instances for all transformers to MonadGen

--- src/Control/Monad/Gen/Class.hs
+++ src/Control/Monad/Gen/Class.hs
@@ -6,17 +6,21 @@
 module Control.Monad.Gen.Class where
 -- Import the non-depricated one
 #if MIN_VERSION_mtl(2, 2, 0)
-import Control.Monad.Except
+import           Control.Monad.Except
 #else
-import Control.Monad.Trans.Error
+import           Control.Monad.Trans.Error
 #endif
 
-import Control.Monad.Trans.Identity
-import Control.Monad.Trans.Maybe
-import Control.Monad.List
-import Control.Monad.Reader
-import Control.Monad.State
-import Control.Monad.Writer
+import           Control.Monad.Cont
+import           Control.Monad.List
+import           Control.Monad.RWS
+import           Control.Monad.Reader
+import           Control.Monad.State
+import qualified Control.Monad.State.Strict as SS
+import           Control.Monad.Trans.Identity
+import           Control.Monad.Trans.Maybe
+import           Control.Monad.Writer
+import qualified Control.Monad.Writer.Strict as SW
 
 -- | The MTL style class for generating fresh values
 class Monad m => MonadGen e m | m -> e where
@@ -35,6 +39,14 @@ instance (MonadGen e m, Monoid s) => MonadGen e (WriterT s m)  where
 instance MonadGen e m => MonadGen e (ListT m) where
   gen = lift gen
 instance MonadGen e m => MonadGen e (MaybeT m) where
+  gen = lift gen
+instance MonadGen e m => MonadGen e (ContT r m) where
+  gen = lift gen
+instance (Monoid w, MonadGen e m) => MonadGen e (RWST r w s m) where
+  gen = lift gen
+instance MonadGen e m => MonadGen e (SS.StateT s m) where
+  gen = lift gen
+instance (Monoid w, MonadGen e m) => MonadGen e (SW.WriterT w m) where
   gen = lift gen
 
 #if MIN_VERSION_mtl(2, 2, 0)
